e75776d5bf9b7622ff063df268344dbde5b478b7
Ghodjeezreal <fredrickokojie@yahoo.com>
2025-12-29 20:34:54 -0800

    prouction json added

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 const prismaClientSingleton = () => {
   const accelerateUrl = process.env.DATABASE_URL;
